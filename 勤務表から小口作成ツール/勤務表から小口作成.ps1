3d19bce68ad8499a5fe68272f9887041d330cb27
Natsumi Matsuzawa <matsuzawa-nm@bhopari.onmicrosoft.com>
2021-05-31 09:38:53 +0900

    設計書作成、ps1　コメントと小口を複製するところまで動作確認済み

---
+++ 勤務表から小口作成ツール/勤務表から小口作成.ps1
@@ -0,0 +1,104 @@
+#
+# 勤務表から小口を作成するツール
+# 
+# 
+# お台場、田町の場合のみ小口を記入
+# 15日分記入可能
+#
+# 用意するもの
+# ・対象の勤務表
+#       形式: <3桁の社員番号>_勤務表_m月_<名前>.xlsx
+# ・小口計算のテンプレート
+#       形式: <3桁の社員番号>_小口交通費・出張旅費精算明細書_<氏名>_テンプレ.xlsx
+#       所属、氏名、印鑑は記入しておく
+# 
+
+# =======1.ユーザーに「何月のにします？」対話型で聞く=======
+# =======2.対象月を入力=======
+$nanngatsu = Read-Host "何月の小口を作成しますか？(半角数字で入力)"
+
+
+# =======3.対象の勤務表をINPUTとして受け取る=======
+$kinmuhyou = gci -Recurse | ? name -CMatch "[0-9]{3}_勤務表_($nanngatsu)月_.+"
+$kinmuhyou
+
+if($kinmuhyou -eq $null){
+    echo ($nanngatsu + "月の勤務表を用意してください")
+} else {
+    echo ($nanngatsu +"月の小口を作成します")
+}
+
+# =======4.小口に入力=======
+# すでにあるExcelのプロセスをつかむ
+Add-Type -AssemblyName Microsoft.office.Interop.Excel
+try{
+    $excel = [Runtime.InteropServices.Marshal]::GetActiveObject('Excel.Application')
+} catch {
+    # なければ新規を作る
+    $excel = New-Object -ComObject Excel.Application
+}
+
+$excel.visible = $true
+
+# 勤務表のテンプレをつかむ
+$kinmuhyouBook = $excel.workbooks.open($kinmuhyou.fullname)
+
+# 小口のテンプレをつかむ
+$koguchiTemple = gci -Recurse | ? name -CMatch "[0-9]{3}_小口交通費・出張旅費精算明細書_.+_テンプレ"
+echo ($koguchiTemple + "をテンプレートとします")
+
+$koguchiTempleBook = $excel.workbooks.open($koguchiTemple.fullname)
+
+# 小口を複製する
+$koguchiBook = $excel.Workbooks.add()
+
+# ☆☆ここから☆☆
+$koguchiBook.worksheets(1).range("A1:AH68") = $koguchiTempleBook.worksheets(1).range("A1:AH68")
+
+# ファイル名変更 → これは自分でやってもらおうかなあ
+# Rename-Item '119_小口交通費・出張旅費精算明細書_名前.xlsx' "119_小口交通費・出張旅費精算明細書_名前.xlsx"
+
+# データ取得対象シートを指定する
+
+# お台場、田町の場合のみ小口を記入
+# 「勤務内容」 or 「備考」にお台場、田町があったらループに入る
+# if
+
+# ★ループ開始★
+
+
+# 「月」に記入
+# B11、14、17...にユーザーが入力した対象月を入れる
+
+# 「日」に記入
+# C列をコピペ
+
+# 「適用（行先、要件）」に記入
+# 田町：自宅（生田）?田町
+# お台場：自宅（生田）?作業（お台場）
+
+# 「区間」に記入
+
+# 「交通機関」に記入
+
+# 「金額」に記入
+
+
+# ★ループ終了★
+
+# 53行目じゃなかったら「適用（行先、要件）」に「以下余白」記入
+
+
+# H60に対象月を入力
+
+# K60に月末日を入力
+
+
+# bookを保存
+$kinmuhyouBook.save()
+$koguchiTempleBook.save()
+$koguchiBook.saveas("C:\Users\bvs20002\Documents\010_自習の回\06_powershell-lesson\勤務表から小口作成ツール\暫定.xlsx")
+
+# Excelを閉じる
+
+# 変数の解放
